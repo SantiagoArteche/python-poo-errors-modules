4407a40283282e55923ce56675ad4fb23686ac80
santiago arteche <santiarteche@hotmail.com>
2024-08-28 23:54:14 -0300

    first course

---
+++ module.py
@@ -0,0 +1,6 @@
+import practice
+
+person2 = practice.Person('Santiago', 'Arteche', 25, 'Bra')
+person2.printPerson('Class called from module.py')
+factorialFive = person2.factorial(5)
+print(factorialFive)
---
+++ practice.py
@@ -0,0 +1,31 @@
+class Person:
+    def __init__(self, name, surname, age, country = 'Arg') -> None:
+        self.__name = name
+        self.__surname = surname
+        self.age = age
+        self.country = country
+    def printPerson(self, newParameter = 'SelfAssigned'):
+        print(f"First name: {self.__name} \nSurname: {self.__surname} \nAge: {self.age}\n{self.country}\n{newParameter}")
+
+    def factorial(self, n):
+        if(n == 0): 
+            return 1
+        
+        return n * self.factorial(n - 1)
+
+person1 = Person('Santiago', 'Arteche', 25)
+person1.newProperty = 'This is a new property'
+person1.printPerson('Man')
+
+my_tuple = tuple([1,2,3,4,5,6])
+try:
+    my_tuple[0] = 0
+    print(my_tuple)
+except ValueError as error:
+    print(error)
+except Exception as exception:
+    print(f"ERROR: {exception}")
+else:
+    print('Continue the ejecution')
+finally:
+    print('Program finished')
